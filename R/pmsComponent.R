5b4d05529d59bb44e5054c734a318e6f7a07742d
bluesky49 <goming49@gmail.com>
2020-09-12 18:42:54 +0800

    final update

--- R/pmsComponent.R
+++ R/pmsComponent.R
@@ -1,16 +1,16 @@
 # AUTO GENERATED FILE - DO NOT EDIT
 
-pmsComponent <- function(value=NULL) {
+pMSComponent <- function(value=NULL, id=NULL, newGirl=NULL, updateData=NULL, range=NULL, deleteID=NULL) {
     
-    props <- list(value=value)
+    props <- list(value=value, id=id, newGirl=newGirl, updateData=updateData, range=range, deleteID=deleteID)
     if (length(props) > 0) {
         props <- props[!vapply(props, is.null, logical(1))]
     }
     component <- list(
         props = props,
-        type = 'pmsComponent',
+        type = 'PMSComponent',
         namespace = 'dash_extra_ui_components',
-        propNames = c('value'),
+        propNames = c('value', 'id', 'newGirl', 'updateData', 'range', 'deleteID'),
         package = 'dashExtraUiComponents'
         )
 
--- dash_extra_ui_components/pmsComponent.py
+++ dash_extra_ui_components/pmsComponent.py
@@ -3,19 +3,24 @@
 from dash.development.base_component import Component, _explicitize_args
 
 
-class pmsComponent(Component):
-    """A pmsComponent component.
+class PMSComponent(Component):
+    """A PMSComponent component.
 
 
 Keyword arguments:
-- value (list; optional)"""
+- value (string; optional)
+- id (string; optional)
+- newGirl (dict; optional)
+- updateData (dict; optional)
+- range (number; default 21)
+- deleteID (string; optional)"""
     @_explicitize_args
-    def __init__(self, value=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['value']
-        self._type = 'pmsComponent'
+    def __init__(self, value=Component.UNDEFINED, id=Component.UNDEFINED, newGirl=Component.UNDEFINED, updateData=Component.UNDEFINED, range=Component.UNDEFINED, deleteID=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['value', 'id', 'newGirl', 'updateData', 'range', 'deleteID']
+        self._type = 'PMSComponent'
         self._namespace = 'dash_extra_ui_components'
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['value']
+        self.available_properties = ['value', 'id', 'newGirl', 'updateData', 'range', 'deleteID']
         self.available_wildcard_properties =            []
 
         _explicit_args = kwargs.pop('_explicit_args')
@@ -27,4 +32,4 @@ Keyword arguments:
             if k not in args:
                 raise TypeError(
                     'Required argument `' + k + '` was not specified.')
-        super(pmsComponent, self).__init__(**args)
+        super(PMSComponent, self).__init__(**args)
